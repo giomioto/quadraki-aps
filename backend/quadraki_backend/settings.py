d5a19534ef9b076781dd4a74d7c9f876f06dd7e3
Giovanni Mioto <89156343+giomioto@users.noreply.github.com>
2026-06-16 20:43:43 -0300

    Update settings.py

--- backend/quadraki_backend/settings.py
+++ backend/quadraki_backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'quadraki',
         'USER': 'postgres',
-        'PASSWORD': 'Metallica13@', # Coloque a senha que configurou na instalação
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
